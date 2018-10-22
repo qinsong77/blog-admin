a3c5a49c8a8a58562b652f070b8d2e8c674c4f09
work_desktop <xyqinsong@163.com>
2018-10-22 11:54:28 +0800

    test commit 333

--- src/views/layout/components/slidebar.vue
+++ src/views/layout/components/slidebar.vue
@@ -21,6 +21,7 @@
                 <el-submenu index="1-4">
                     <span slot="title">选项4</span>
                     <el-menu-item index="1-4-1">选项1</el-menu-item>
+                    <el-menu-item index="1-4-2">选项2</el-menu-item>
                 </el-submenu>
             </el-submenu>
             <el-menu-item index="2">
