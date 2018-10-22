ab7ae1a450392b8c50a3baffff11513cb08d3ac2
work_desktop <xyqinsong@163.com>
2018-10-22 10:52:28 +0800

    test work computer can commit“

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import 'normalize.css/normalize.css'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './style/index.scss'
-
+//test work computer can commit
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
