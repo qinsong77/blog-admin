02dabfe5f06ffaae6e94296f103a6a2e2d798a81
work_desktop <xyqinsong@163.com>
2018-10-22 11:16:07 +0800

    rollback to berfore test desktop commit

--- src/main.js
+++ src/main.js
@@ -6,8 +6,7 @@ import 'normalize.css/normalize.css'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './style/index.scss'
-//test work computer can commit
-//test work computer can commit2
+
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
