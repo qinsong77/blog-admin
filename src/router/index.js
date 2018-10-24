c4432f0a0dcd8b1ae89590420c6de8ad7a70395e
work_desktop <xyqinsong@163.com>
2018-10-24 16:01:56 +0800

    '路由错误判断'

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import routes from './routes'
-import {getToken} from '../common/auth'
+import { getToken } from '../common/auth'
+
+import { Notification } from 'element-ui'
 // 进度条
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
@@ -9,38 +11,45 @@ import 'nprogress/nprogress.css'
 Vue.use(Router)
 
 const router = new Router({
-    mode: 'hash',
-    base: process.env.BASE_URL,
-    scrollBehavior: () => ({y: 0}),
-    routes: routes
+  mode: 'hash',
+  base: process.env.BASE_URL,
+  scrollBehavior: () => ({ y: 0 }),
+  routes: routes
 })
 let startTime
 router.beforeEach((to, from, next) => {
-    NProgress.start()
-    startTime = new Date();
-    if (getToken()) {
-        if (to.path === '/login') {
-            next({path: '/'})
-        } else {
-            next()
-        }
+  NProgress.start()
+  startTime = new Date()
+  if (getToken()) {
+    if (to.path === '/login') {
+      next({ path: '/' })
     } else {
-        if (to.path === '/login') {
-            next()
-        } else {
-            next('/login')
-        }
+      next()
     }
+  } else {
+    if (to.path === '/login') {
+      next()
+    } else {
+      next('/login')
+    }
+  }
 })
 
 router.afterEach((to) => {
-    document.title = to.meta.title || 'Sysuke\'s Blog Admin'
-    if (new Date() - startTime > 1000) {
-        NProgress.done()
-    } else {
-        setTimeout(() => {
-            NProgress.done()
-        },new Date() - startTime + 300)
-    }
+  document.title = to.meta.title || 'Sysuke\'s Blog Admin'
+  if (new Date() - startTime > 1000) {
+    NProgress.done()
+  } else {
+    setTimeout(() => {
+      NProgress.done()
+    }, new Date() - startTime + 300)
+  }
+})
+router.onError((err) => {
+  NProgress.done()
+  Notification.error({
+    title: '路由发生错误',
+    message: err
+  })
 })
 export default router
